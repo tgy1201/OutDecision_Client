a01b4d7aa7b9d0bc4bb6b49e2cfa7f0701ab3f82
siyeon117 <127119016+siyeon117@users.noreply.github.com>
2024-04-08 03:43:53 +0900

    수정

--- src/pages/ranking/Ranking.js
+++ src/pages/ranking/Ranking.js
@@ -172,156 +172,150 @@ function Ranking() {
             </div>
 
             <div className={styles.mobile_ranking}>
-                <div className={styles.wrap}>
-                    <div className={styles.containerinner}>
-                        <div className={styles.divinner}>
-                            <div className={styles.contentswrap}>
-                                <h1 className={styles.contitle}>
-                                    랭킹은 매주 월요일 00:00에 갱신됩니다
-                                </h1>
-                                <div>
-                                    <div className={styles.ranktablewrap}>
-                                        <table className={styles.ranktable}>
-                                            <thead className={styles.rankhead}>
-                                                <tr>
-                                                    <th>랭킹</th>
-                                                    <th>닉네임</th>
-                                                    <th>포인트</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <td>순위</td>
-                                                    <td>
-                                                        <div className={styles.profile}>
-                                                            <img src="/assets/images/profile.png" alt="프로필" />
-                                                        </div>
-                                                        내가일등
-                                                    </td>
-                                                    <td>75608</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>순위</td>
-                                                    <td>
-                                                        <div className={styles.profile}>
-                                                            <img src="/assets/images/profile.png" alt="프로필" />
-                                                        </div>
-                                                        내가일등
-                                                    </td>
-                                                    <td>75608</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>순위</td>
-                                                    <td>
-                                                        <div className={styles.profile}>
-                                                            <img src="/assets/images/profile.png" alt="프로필" />
-                                                        </div>
-                                                        내가일등
-                                                    </td>
-                                                    <td>75608</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>순위</td>
-                                                    <td>
-                                                        <div className={styles.profile}>
-                                                            <img src="/assets/images/profile.png" alt="프로필" />
-                                                        </div>
-                                                        내가일등
-                                                    </td>
-                                                    <td>75608</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>순위</td>
-                                                    <td>
-                                                        <div className={styles.profile}>
-                                                            <img src="/assets/images/profile.png" alt="프로필" />
-                                                        </div>
-                                                        내가일등
-                                                    </td>
-                                                    <td>75608</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>순위</td>
-                                                    <td>
-                                                        <div className={styles.profile}>
-                                                            <img src="/assets/images/profile.png" alt="프로필" />
-                                                        </div>
-                                                        내가일등
-                                                    </td>
-                                                    <td>75608</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>순위</td>
-                                                    <td>
-                                                        <div className={styles.profile}>
-                                                            <img src="/assets/images/profile.png" alt="프로필" />
-                                                        </div>
-                                                        내가일등
-                                                    </td>
-                                                    <td>75608</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>순위</td>
-                                                    <td>
-                                                        <div className={styles.profile}>
-                                                            <img src="/assets/images/profile.png" alt="프로필" />
-                                                        </div>
-                                                        내가일등
-                                                    </td>
-                                                    <td>75608</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>순위</td>
-                                                    <td>
-                                                        <div className={styles.profile}>
-                                                            <img src="/assets/images/profile.png" alt="프로필" />
-                                                        </div>
-                                                        내가일등
-                                                    </td>
-                                                    <td>75608</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>순위</td>
-                                                    <td>
-                                                        <div className={styles.profile}>
-                                                            <img src="/assets/images/profile.png" alt="프로필" />
-                                                        </div>
-                                                        내가일등
-                                                    </td>
-                                                    <td>75608</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>순위</td>
-                                                    <td>
-                                                        <div className={styles.profile}>
-                                                            <img src="/assets/images/profile.png" alt="프로필" />
-                                                        </div>
-                                                        내가일등
-                                                    </td>
-                                                    <td>75608</td>
-                                                </tr>
-                                            </tbody>
-                                            <tfoot>
-                                                <tr>
-                                                    <td>순위</td>
-                                                    <td>
-                                                        <div className={styles.profile}>
-                                                            <img src="/assets/images/profile.png" alt="프로필" />
-                                                        </div>
-                                                        정감자
-                                                    </td>
-                                                    <td>117</td>
-                                                </tr>
-                                            </tfoot>
-                                        </table>
-                                    </div>
-                                    <div className={styles.rankinfowrap}>
-                                        <table className={styles.rankinfo}>
-                                            일일랭킹보상 주간랭킹보상
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
+                <div className={styles.contentswrap}>
+                    <h1 className={styles.contitle}>
+                        랭킹은 매주 월요일 00:00에 갱신됩니다
+                    </h1>
+                    <div>
+                        <div className={styles.ranktablewrap}>
+                            <table className={styles.ranktable}>
+                                <thead className={styles.rankhead}>
+                                    <tr>
+                                        <th>랭킹</th>
+                                        <th>닉네임</th>
+                                        <th>포인트</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>순위</td>
+                                        <td>
+                                            <div className={styles.profile}>
+                                                <img src="/assets/images/profile.png" alt="프로필" />
+                                            </div>
+                                            내가일등
+                                        </td>
+                                        <td>75608</td>
+                                    </tr>
+                                    <tr>
+                                        <td>순위</td>
+                                        <td>
+                                            <div className={styles.profile}>
+                                                <img src="/assets/images/profile.png" alt="프로필" />
+                                            </div>
+                                            내가일등
+                                        </td>
+                                        <td>75608</td>
+                                    </tr>
+                                    <tr>
+                                        <td>순위</td>
+                                        <td>
+                                            <div className={styles.profile}>
+                                                <img src="/assets/images/profile.png" alt="프로필" />
+                                            </div>
+                                            내가일등
+                                        </td>
+                                        <td>75608</td>
+                                    </tr>
+                                    <tr>
+                                        <td>순위</td>
+                                        <td>
+                                            <div className={styles.profile}>
+                                                <img src="/assets/images/profile.png" alt="프로필" />
+                                            </div>
+                                            내가일등
+                                        </td>
+                                        <td>75608</td>
+                                    </tr>
+                                    <tr>
+                                        <td>순위</td>
+                                        <td>
+                                            <div className={styles.profile}>
+                                                <img src="/assets/images/profile.png" alt="프로필" />
+                                            </div>
+                                            내가일등
+                                        </td>
+                                        <td>75608</td>
+                                    </tr>
+                                    <tr>
+                                        <td>순위</td>
+                                        <td>
+                                            <div className={styles.profile}>
+                                                <img src="/assets/images/profile.png" alt="프로필" />
+                                            </div>
+                                            내가일등
+                                        </td>
+                                        <td>75608</td>
+                                    </tr>
+                                    <tr>
+                                        <td>순위</td>
+                                        <td>
+                                            <div className={styles.profile}>
+                                                <img src="/assets/images/profile.png" alt="프로필" />
+                                            </div>
+                                            내가일등
+                                        </td>
+                                        <td>75608</td>
+                                    </tr>
+                                    <tr>
+                                        <td>순위</td>
+                                        <td>
+                                            <div className={styles.profile}>
+                                                <img src="/assets/images/profile.png" alt="프로필" />
+                                            </div>
+                                            내가일등
+                                        </td>
+                                        <td>75608</td>
+                                    </tr>
+                                    <tr>
+                                        <td>순위</td>
+                                        <td>
+                                            <div className={styles.profile}>
+                                                <img src="/assets/images/profile.png" alt="프로필" />
+                                            </div>
+                                            내가일등
+                                        </td>
+                                        <td>75608</td>
+                                    </tr>
+                                    <tr>
+                                        <td>순위</td>
+                                        <td>
+                                            <div className={styles.profile}>
+                                                <img src="/assets/images/profile.png" alt="프로필" />
+                                            </div>
+                                            내가일등
+                                        </td>
+                                        <td>75608</td>
+                                    </tr>
+                                    <tr>
+                                        <td>순위</td>
+                                        <td>
+                                            <div className={styles.profile}>
+                                                <img src="/assets/images/profile.png" alt="프로필" />
+                                            </div>
+                                            내가일등
+                                        </td>
+                                        <td>75608</td>
+                                    </tr>
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <td>순위</td>
+                                        <td>
+                                            <div className={styles.profile}>
+                                                <img src="/assets/images/profile.png" alt="프로필" />
+                                            </div>
+                                            정감자
+                                        </td>
+                                        <td>117</td>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                        <div className={styles.rankinfowrap}>
+                            <table className={styles.rankinfo}>
+                                일일랭킹보상 주간랭킹보상
+                            </table>
                         </div>
                     </div>
                 </div>
