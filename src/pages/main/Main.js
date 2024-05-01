a4528ad4c1f9cad67ebe5c33a213c28040aad5b5
tgy1201 <dndsm82@kyonggi.ac.kr>
2024-05-01 22:24:23 +0900

    메인페이지 핫,최신 게시글 구현

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -3,28 +3,24 @@ import styles from "./main.module.css"
 import Banner from "../../component/banner/Banner";
 import Category from "../../component/category/Category";
 import Recommend from "../../component/recommend/Recommend";
-import Hot from "../../component/hot/Hot";
 import axios from "axios";
+import MainPost from "../../component/mainPost/MainPost";
+
+import { FiChevronRight } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 function Main () {
-    const [hotPosts, setHotPosts] = useState([]);
     const [recommendPosts, setRecommendPosts] = useState([]);
+    const [hotPosts, setHotPosts] = useState([]);
+    const [newPosts, setNewPosts] =  useState([]);
 
     useEffect(() => {
+        handlefetchRecommendPosts();
         handlefetchHotPosts();
-        handleRefreshRecommendPosts();
+        handlefetchNewPosts();
     }, []);
 
-    const handlefetchHotPosts = async () => {
-        try {
-            const response = await axios.get('/assets/data/posts.json');
-            setHotPosts(response.data.posts);
-        } catch(error) {
-            console.log(error);
-        }
-    };
-    
-    const handleRefreshRecommendPosts = async () => {
+    const handlefetchRecommendPosts = async () => {
         try {
             const response = await axios.get('/assets/data/posts.json');
             setRecommendPosts(response.data.posts);
@@ -32,9 +28,26 @@ function Main () {
             console.log(error);
         }
     };
+
+    const handlefetchHotPosts = async () => {
+        try {
+            const response = await axios.get('/assets/data/posts.json');
+            setHotPosts(response.data.posts.slice(0,4));
+        } catch(error) {
+            console.log(error);
+        }
+    };
     
+    const handlefetchNewPosts = async () => {
+        try {
+            const response = await axios.get('/assets/data/posts.json');
+            setNewPosts(response.data.posts.slice(0,4));
+        } catch(error) {
+            console.log(error);
+        }
+    };
+
     return (
-        <>
         <div className={styles.container}>
             <Banner />
             <div className={styles.contents}>
@@ -43,20 +56,31 @@ function Main () {
                     <Category />
                 </section>
                 <section className={styles.recommend}>
-                    <Recommend posts={recommendPosts} handleClick={handleRefreshRecommendPosts}/>
+                    <Recommend posts={recommendPosts} handleClick={handlefetchRecommendPosts}/>
                 </section>
                 <section className={styles.hot}>
-                    <Hot posts={hotPosts}/>
+                    <div className={styles.post_header}>
+                        <p>지금 뜨고 있는 투표는?</p>
+                        <Link to="/board/hot">
+                            <div>HOT 게시물<FiChevronRight style={{verticalAlign: 'middle', fontSize: "1.8rem"}}/></div>
+                        </Link>  
+                    </div>
+                    <MainPost posts={hotPosts} bname={'hot'}/>
                 </section>
                 <section className={styles.all}>
-                    최신게시물
+                    <div className={styles.post_header}>
+                        <p>따끈따끈 방금 올라온 투표는?</p>
+                        <Link to="/board/all">
+                            <div>최신 게시물<FiChevronRight style={{verticalAlign: 'middle', fontSize: "1.8rem"}}/></div>
+                        </Link>  
+                    </div>
+                    <MainPost posts={newPosts} bname={'all'}/>
                 </section>
                 <section className={styles.end}>
                     종료된 게시물
                 </section>
             </div>
         </div>
-        </>
     )
 }
 
