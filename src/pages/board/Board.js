7c12fd6c8dee74a3d383a6eb3348d226d7b6b451
tgy1201 <dndsm82@kyonggi.ac.kr>
2024-05-10 01:36:38 +0900

    게시판 쿼리스트링 수정

--- src/pages/board/Board.js
+++ src/pages/board/Board.js
@@ -36,11 +36,14 @@ function Board ({setCategory}) {
     const navigate = useNavigate();
 
     const [searchParams] = useSearchParams();
-    const type = searchParams.get('type');
-    const hot = searchParams.get('mode');
-    const gender = searchParams.get('gender');
-    const vote = searchParams.get('vote');
-    const page = searchParams.get('page');
+    const type = searchParams.get('type'); // 게시판 정렬 유형(dashboard, list)
+    const mode = searchParams.get('mode'); // 게시판 모드(normal, hot)
+    const gender = searchParams.get('gender'); // 필터 성별(male, female)
+    const vote = searchParams.get('vote'); // 필터 투표상태(progress, end)
+    const page = searchParams.get('page'); // 페이지번호
+    //const search = searchParams.get('search'); // 검색어
+    //const searchType = searchParams.get('searchType'); // 검색 유형(title, content)
+    //const sort = searchParams.get('sort'); // 게시글 정렬(latest, likes, views)
 
     const {bname} = useParams();
 
@@ -97,29 +100,22 @@ function Board ({setCategory}) {
     }, [gender, vote, posts]);
 
     const handleTypeParams = (type) => {
+        /* type 변경 시 mode, gender, vote, search, searchType, sort 유지(있으면) / page 초기화 */
         searchParams.set('type', type);
         searchParams.delete('page'); //페이지 초기화
         navigate(`?${searchParams.toString()}`)
     }
 
-    const handleChangeBoard = (board) => {
-        if (type) {
-            if (board === 'hot') {
-                navigate(`?mode=hot&type=${type}`);
-            } else {
-                navigate(`?type=${type}`);
-            }
-        } else {
-            if (board === 'hot') {
-                navigate(`?mode=hot&type=dashboard`);
-            } else {
-                navigate(`?type=dashboard`);
-            }
-        }
+    const handleChangeBoard = (modeValue) => {
+        navigate(
+            modeValue === 'hot'
+            ? type ? `?mode=hot&type=${type}` : `?mode=hot&type=dashboard`
+            : type ? `?mode=normal&type=${type}` : `?mode=normal&type=dashboard`
+        )
     }
 
     const applyFilter = (filterType, value) => {
-        // 여기에서 필터를 적용하고 게시판을 다시 렌더링하거나 데이터를 업데이트할 수 있음
+        /* 쿼리스트링에 fiterType 매개변수 추가 */
         if (filterType === 'gender') {
             value && searchParams.set('gender', value);
         } else if (filterType === 'voteStatus') {
@@ -128,6 +124,7 @@ function Board ({setCategory}) {
     };
     
     const deleteFilter = (filterType) => {
+        /* 쿼리스트링에서 fiterType에 해당하는 매개변수 제거 후 이동 */
         if (filterType === 'gender') {
             searchParams.delete('gender');
         } else if (filterType === 'voteStatus') {
@@ -144,8 +141,8 @@ function Board ({setCategory}) {
                 </div>
                 <div className={styles.board_nav}>
                     <div>
-                        <button className={hot ? `${styles.unselect}` : `${styles.select}`} onClick={()=>handleChangeBoard()}>전체글</button>
-                        <button className={hot ? `${styles.select}` : `${styles.unselect}`} onClick={()=>handleChangeBoard('hot')} style={{display: bname === 'hot' || bname === 'all' ? "none" : ""}}>HOT</button>
+                        <button className={!mode || mode === 'normal' ? `${styles.select}` : `${styles.unselect}`} onClick={()=>handleChangeBoard('normal')}>전체글</button>
+                        <button className={mode === 'hot' ? `${styles.select}` : `${styles.unselect}`} onClick={()=>handleChangeBoard('hot')} style={{display: bname === 'hot' || bname === 'all' ? "none" : ""}}>HOT</button>
                     </div>
                     <Link to="/write" className={styles.write_btn} style={{display: bname === 'hot' && "none"}}>
                         <div className={styles.write_img}>
@@ -161,6 +158,7 @@ function Board ({setCategory}) {
                             <option>조회순</option>
                             <option>좋아요순</option>
                         </select>
+
                         <button className={styles.filter_btn} onClick={()=> setFilterOpen(!filterOpen)} style={{borderColor: gender || vote ? "#ac2323" : ""}}>
                             <span>필터</span>
                             <div>
@@ -189,6 +187,8 @@ function Board ({setCategory}) {
                             searchParams={searchParams}
                         />
                         }
+
+                        {/* [데스크톱]필터 적용시 필터 내용 요약*/}
                         {gender && !isMobile &&
                             <div className={styles.gender_btn}>
                                 {filterMap[gender]} 
@@ -202,6 +202,8 @@ function Board ({setCategory}) {
                             </div>
                         }
                     </div>
+
+                    {/* 정렬 버튼 네비게이션 */}
                     <div className={styles.sorting_btn}>
                         <button onClick={()=> handleTypeParams('dashboard')}>
                         {type !== 'list' ?
@@ -216,7 +218,10 @@ function Board ({setCategory}) {
                         }
                         </button>
                     </div>
+
                 </div>
+
+                {/* [모바일]필터 적용시 필터 내용 요약*/}
                 <div className={styles.mobile_filter} style={{display: gender && vote === "" ? "none" : ""}}>
                     {gender && isMobile &&
                         <div className={styles.gender_btn}>
@@ -232,11 +237,13 @@ function Board ({setCategory}) {
                     }
                 </div>
 
+                {/* type이 없거나 dashboard면 Dashboad 컴포넌트로, type이 list면 List 컴포넌트로 렌더링*/}
                 {!type || type === 'dashboard' 
                 ? <Dashboard posts={filteredPosts} bname={bname} page={page}/> 
                 : <List posts={filteredPosts} bname={bname} page={page}/>
                 }
 
+                {/* 검색바 */}
                 <div className={styles.search_wrap}>
                     <select>
                         <option>제목</option>
